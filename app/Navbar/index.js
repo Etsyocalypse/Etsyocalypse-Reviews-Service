46c6eeba5658798df32b3f086745474ac7389b7e
Tim Hedge <44926661+timhedge@users.noreply.github.com>
2020-03-10 16:25:34 -0500

    Add files via upload

---
+++ app/Navbar/index.js
@@ -0,0 +1,17 @@
+
+import React from 'react'
+
+class Navbar extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <h1>WOWWWW</h1>
+        );
+    }
+
+}
+
+export default Navbar;
---
+++ app/index.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
+import Navbar from './Navbar'
+
+render(<Navbar/>, document.getElementById('navbar'))
