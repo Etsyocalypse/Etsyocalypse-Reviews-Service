5d1b8221b435c3cbd2b8efc7f771a6283df40b57
Tim Hedge <timwhedge@gmail.com>
2020-03-11 12:03:03 -0500

    Replace boilerplate

--- client/Task.js
+++ client/Task.js
@@ -1,12 +1,1 @@
-import React from 'react';
 
-const Task = (props) => {
-  return (
-    <>
-      <ul>{props.tasks.map((task, index) => <li onClick={props.deleteItem} key={index} data-id={task.id}>{task.task}</li>)}</ul>
-    </>
-  )
-}
-
-
-export default Task;
