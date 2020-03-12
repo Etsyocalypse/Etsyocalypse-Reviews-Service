d4985522b74974d3130e41962d397f9df628924d
Tim Hedge <timwhedge@gmail.com>
2020-03-12 17:32:22 -0500

    adds animation to tabs

--- client/App.js
+++ client/App.js
@@ -19,12 +19,20 @@ export default class Reviews extends React.Component {
     }
 
     handleItemReviewTabClick() {
+        let shopTab = document.getElementById('tabShop');
+        shopTab.classList.remove('selectedTab');
+        let itemTab = document.getElementById('tabItem');
+        itemTab.classList.add('selectedTab');
         this.setState({
             tab: 'item'
         });
     }
 
     handleShopReviewTabClick() {
+        let itemTab = document.getElementById('tabItem');
+        itemTab.classList.remove('selectedTab');
+        let shopTab = document.getElementById('tabShop');
+        shopTab.classList.add('selectedTab');
         this.setState({
             tab: 'shop'
         })
@@ -54,17 +62,19 @@ export default class Reviews extends React.Component {
             <div className="review main">
                 <div className="review ratings">
                     <div className="review shop">
-                      <p className="review shopRating">Shop rating</p>
+                      <p className="review rating-headers shop-rating">Shop rating</p>
                       <ShopStars calcStars={this.calculateStarRatingAvg} shopReviewData={this.state.shopReviewData}/>
                     </div>
                     <div className="review shop">
-                        <p className="review shopReview">Total shop reviews</p>
+                        <p className="review rating-headers shop-review">Total shop reviews</p>
                         <p className="review shopReviewNum">{this.state.shopReviewData.length}</p>
                     </div>
                 </div>
-                <div className="review tabs">
-                    <button className="tab" onClick={this.handleItemReviewTabClick}>Reviews for this item<span className="reviewNum">{this.state.itemReviewData.length}</span></button>
-                    <button className="tab" onClick={this.handleShopReviewTabClick}>Reviews for this shop<span className="reviewNum">{this.state.shopReviewData.length}</span></button>
+                <div id="review-tab-container">
+                    <div id="tabs">
+                        <button id="tabItem" className="tabAnimate" onClick={this.handleItemReviewTabClick}>Reviews for this item<span className="reviewNum">{this.state.itemReviewData.length}</span></button>
+                        <button id="tabShop" className="tabAnimate" onClick={this.handleShopReviewTabClick}>Reviews for this shop<span className="reviewNum">{this.state.shopReviewData.length}</span></button>
+                    </div>
                 </div>
                 <div className="review reviewList" ><ReviewList itemReviewData={this.state.itemReviewData} shopReviewData={this.state.shopReviewData} tab={this.state.tab}/></div>
                 <div className="review pictures">
