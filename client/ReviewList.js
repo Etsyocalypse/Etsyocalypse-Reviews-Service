ccf6d3cde0d086c46c8a8b3263089f7d49d85032
Tim Hedge <timwhedge@gmail.com>
2020-03-13 12:30:34 -0500

    Updates avatar placeholder image

--- client/ReviewList.js
+++ client/ReviewList.js
@@ -8,8 +8,8 @@ const ReviewList = (props) => {
             {props.itemReviewData.map((rev, index) => (
                 <div data-id={rev.id} key={index} className="review-container">
                     <div className="topline">
-                        <div className="avatar"><img className="avatar-img" src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png" alt="dummyAvatar"></img></div>
-                        <p><a href="">{rev.reviewer}</a>{'  ' + rev.date}</p>
+                        <div className="avatar"><i className="fas fa-user-circle"></i></div>
+                        <p><a href="">{rev.reviewer}</a>&nbsp;&nbsp;{rev.date}</p>
                     </div>
                     <div className="review-body">
                         <div className="star-text">
