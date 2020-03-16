321a94c8431511dd9f962af83ed8c9bf56c2c5f4
Tim Hedge <timwhedge@gmail.com>
2020-03-16 17:41:12 -0500

    Adds some basic outline for the MongoDB setup

--- server/db.js
+++ server/db.js
@@ -1,9 +1,27 @@
-var mongoose = require('mongoose');
-var db = mongoose.connection;
+const mongoose = require('mongoose');
+const db = mongoose.connection;
+const Schema = mongoose.Schema;
 
 mongoose.connect('mongodb://localhost/reviews_db', {useNewUrlParser: true});
+
+const reviewSchema = new Schema ({
+  productId: Number,
+  shopId: Number,
+  type: String,
+  avatar: String,
+  reviewer: String,
+  date: Date,
+  star_rating: Number,
+  text: String,
+  imageURL: String,
+  item: String,
+  itemImageURL: String,
+});
+
+const reviewModel = mongoose.model('Review', reviewSchema);
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log('connected')
 });
+
